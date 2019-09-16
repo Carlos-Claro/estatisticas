f22f7f50265e9be5039535236e6bf5d39243d7fe
Carlos <carlosclaro79@gmail.com>
2019-09-16 10:59:10 -0300

    busca imoveis e emrpesas

--- app/main.py
+++ app/main.py
@@ -16,20 +16,27 @@ class Estatisticas(object):
             self.localhost = False
             self.URI = 'http://imoveis.powempresas.com/'
         self.inicio = time.time()
-        self.URL_GET = self.URI + 'log_imoveis/'
-        self.URL_POST_LOG_EMPRESA = self.URI + 'log_imoveis/'
+        self.URL_GET = self.URI + 'log_empresas/'
+        self.URL_POST_LOG_EMPRESA = self.URI + 'log_empresa/'
+        self.URL_GET_IMOVEIS = self.URI + 'log_imoveis/'
+        self.URL_POST_LOG_IMOVEL = self.URI + 'log_imovel/'
         
     def get_dia(self,dias):
         da = datetime.datetime.now() - datetime.timedelta(days=int(dias))
+        retorno = []
+        retorno.append(da.strftime('%Y'))
+        retorno.append(da.strftime('%m'))
+        retorno.append(da.strftime('%d'))
         y = int(da.strftime('%Y'))
         m = int(da.strftime('%m'))
         d = int(da.strftime('%d'))
-        return datetime.datetime(y,m,d,0,0).isoformat()
+        return retorno
+        #return datetime.datetime(y,m,d,0,0)
         
     def get_data(self, chave, valor, data):
         add = {}
         add['data'] = data
-        add['id_empresa'] = chave
+        add['id_empresa'] = int(chave)
         add['total_acessos'] = valor['total_empresa']
         del(valor['total_empresa'])
         for ke,va in valor.items():
@@ -38,8 +45,16 @@ class Estatisticas(object):
             add[ke]['imoveis'] = va['imoveis']
         return add
         
+    def get_data_imovel(self, chave, valor, data):
+        add = {}
+        add['data'] = data
+        add['id_imovel'] = int(chave)
+        add['total_acessos'] = valor['total_imovel']
+        del(valor['total_imovel'])
+        return add
+        
     
-    def roda_dia(self, dias):
+    def roda_empresa_dia(self, dias):
         data = self.get_dia(dias)
         g = {'dias':dias}
         itens = requests.get(self.URL_GET,params=g)
@@ -49,16 +64,29 @@ class Estatisticas(object):
                 post = json.dumps(self.get_data(k,v,data))
                 print(post)
                 res = requests.post(self.URL_POST_LOG_EMPRESA,json=post)
-                print(res.status_code)
-                exit()
-        #print(i)
+        self.fim = time.time()
+        print(self.fim-self.inicio)
+        
+    def roda_imovel_dia(self, dias):
+        data = self.get_dia(dias)
+        g = {'dias':dias}
+        itens = requests.get(self.URL_GET_IMOVEIS,params=g)
+        if itens.status_code == 200:
+            i = itens.json()
+            print(i)
+            for k,v in i.items():
+                post = json.dumps(self.get_data_imovel(k,v,data))
+                print(post)
+                #res = requests.post(self.URL_POST_LOG_EMPRESA,json=post)
         self.fim = time.time()
         print(self.fim-self.inicio)
         
         
     def main(self):
-        dias = 1
-        self.roda_dia(dias)
+        
+        self.roda_imovel_dia(90)
+        #for x in range(365,181,-1):
+        #    print(x)
     
 if __name__ == '__main__':
     Estatisticas().main()
